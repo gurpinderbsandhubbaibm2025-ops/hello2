a3e72ddccff7747be0c3bf940366c4f7b45e506b
gurpinderbsandhubbaibm2025-ops <gurpinderbsandhu_bbaibm2025@iar.ac.in>
2026-02-13 14:20:05 +0530

    pytjon pr
    Added a Python script to perform basic tasks including printing a welcome message and calculating the area of a circle.

---
+++ pr1.py
@@ -0,0 +1,18 @@
+                                                                                          practical1
+
+aim - perform following fundamental task to first hand-on in python
+task 1
+
+print welcome messege as "hello world"
+code-print("hello world")
+
+task 2
+
+write a python program that calculate the area of a circle based on the radius enetered by by user
+
+code - r=float(input("enter value of a"))
+pi=3.14
+A =  pi*r*r
+print("area",a)
+output- enter the value of a 8
+area 200.96
